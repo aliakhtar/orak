37eaafeb4b92df9104512736e4bc7c8ebd6d2ed0
Ali <ali.rac200@gmail.com>
2016-02-20 23:57:06 +0500

    fixed main class in pom, updated run script to use mvn exec

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 
 #Use this script for a full run of the scraper. Note: It requires install.sh to be run first if a recompile is required
 
-java -d64 -Xms512m -Xmx4g -jar  orak-1.0-SNAPSHOT-fat.jar -c ../config.json
+git pull
+mvn clean compile
+mvn exec:exec
