6ff4ebce4d46aa83a1159de86b97be1054504dae
Ali <ali.rac200@gmail.com>
2016-02-21 23:01:56 +0500

    working on claims parser, handling rest of claim except snak

--- src/main/java/com/github/aliakhtar/orak/wikidata/ClaimParser.java
+++ src/main/java/com/github/aliakhtar/orak/wikidata/ClaimParser.java
@@ -8,19 +8,96 @@ import java.util.logging.Logger;
 import com.github.aliakhtar.orak.util.Logging;
 import io.vertx.core.json.JsonObject;
 
+import static com.github.aliakhtar.orak.util.Util.isBlank;
+import static com.github.aliakhtar.orak.util.Util.trimAndDownCase;
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
+
 public class ClaimParser implements Callable<Optional<JsonObject>>
 {
     private final Logger log = Logging.get(this);
 
-    public ClaimParser(String propId,
-                       JsonObject claimInput)
+    private final JsonObject input;
+    private Optional<JsonObject> value = empty();
+    public ClaimParser(JsonObject claimInput)
     {
-
+        this.input = claimInput;
     }
 
     @Override
     public Optional<JsonObject> call() throws Exception
     {
+        JsonObject snak = input.getJsonObject("mainsnak");
+        if (snak == null)
+            return empty();
+
+
+        Optional<String> valueType = determineValueTypeAndProcessValue(snak);
+
+        if (! valueType.isPresent())
+            return empty();
+
+        JsonObject result = new JsonObject();
+        result.put("propertyId", snak.getString("property"))
+                .put("dataType", input.getString("datatype"))
+                .put("valueType", determineValueTypeAndProcessValue(snak))
+        ;
+
         return Optional.empty();
     }
+
+    private Optional<String> determineValueTypeAndProcessValue(JsonObject snak)
+    {
+        String snakType = trimAndDownCase(snak.getString("snaktype");
+
+        if (isBlank(snakType))
+            return of("unknown");
+
+        if ("novalue".equals(snakType))
+            return of(  "none" );
+
+        if ("somevalue".equals(snakType)))
+            return of("unknown");
+
+        if (! snakType.equals("value"))
+            return of(snakType);
+
+        String valueType = trimAndDownCase( snak.getString("datatype") );
+
+        if (isBlank(valueType))
+            return empty();
+
+        switch (valueType)
+        {
+            case "monolingualtext":
+                return of( "txt");
+
+            case "string":
+                return  of("str");
+
+            case  "url":
+                return  of("url");
+
+            case  "external-id":
+                return of("externalId");
+
+            case "globe-coordinate":
+                return of("geo");
+
+            case "quantity":
+                return of("quantity");
+
+            case "time":
+                return of("date");
+
+            case "math":
+            case "commonsMedia":
+            case "wikibase-property":
+                return empty();
+
+            default:
+                return of(valueType);
+        }
+    }
+
 }
