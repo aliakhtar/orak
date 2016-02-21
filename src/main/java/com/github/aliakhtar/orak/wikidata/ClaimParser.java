f608e6892cbc4c89277c45b3653db598cd3ddbad
Ali <ali.rac200@gmail.com>
2016-02-22 01:47:30 +0500

    fixing date parsing further

--- src/main/java/com/github/aliakhtar/orak/wikidata/ClaimParser.java
+++ src/main/java/com/github/aliakhtar/orak/wikidata/ClaimParser.java
@@ -156,11 +156,8 @@ public class ClaimParser implements Callable<Optional<JsonObject>>
         if (isBlank(date))
             return empty();
 
-        if (date.contains("-00-")) //month must be between 1-12
-            date = date.replace("-00-","-01-");
-
-        if (date.contains("-00T")) //same for days
-            date = date.replace("-00T","-01T");
+        if (date.contains("-00")) //month & day must be between 1-12
+            date = date.replace("-00","-01");
 
         return of( new JsonObject().put("date", date));
     }
